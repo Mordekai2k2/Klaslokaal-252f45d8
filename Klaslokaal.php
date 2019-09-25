7c1f8f9d553e6624b9e6a81c704fc275c1548e2b
Randy Brouwer <2105070@talnet.nl>
2019-09-25 09:55:48 +0100

    Uitwerking van de exercise

---
+++ Klaslokaal.php
@@ -0,0 +1,9 @@
+<?php
+$array = array(
+    $name1 = "Sjonnie de Wiel",
+    $name2 = "Herman Kaal",
+    $name3 = "Henk de Steen",
+    $name4 = "Inge Kerkhoven",
+    $name5 = "Gert Kruiswijk",
+);
+var_dump($array);
